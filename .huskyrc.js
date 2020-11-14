ed27681fca46407f17af59f00b2e65ac974720ca
kenji7157 <kawake.kk@gmail.com>
2020-11-15 00:46:57 +0900

    test

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@
 // lintで整形後にコミットする
 const preventCommitToMaster = `branch=\`git symbolic-ref HEAD\`
 npm run lint
-git add
+git add .
 if [ "$branch" = "refs/heads/master" ]; then
     echo "\\033[31mDirect commit to master is not allowed.\\033[0m"
     exit 1
