152f46ded0d4a80dbb68bc9e436187f0d8ae4d7c
kenji7157 <kawake.kk@gmail.com>
2020-11-20 01:57:44 +0900

    lint-stagedの導入

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,8 +1,6 @@
 // masterへのコミットも禁止
 // lintで整形後にコミットする
 const preventCommitToMaster = `branch=\`git symbolic-ref HEAD\`
-npm run lint
-git add .
 if [ "$branch" = "refs/heads/master" ]; then
     echo "\\033[31mDirect commit to master is not allowed.\\033[0m"
     exit 1
