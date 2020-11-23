2af1f9d93d7c1969b2636dea2a9e179bef02b11c
kenji7157 <kawake.kk@gmail.com>
2020-11-24 01:09:21 +0900

    再度ナビの実装

--- packages/apps/src/App.vue
+++ packages/apps/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Bola chat👋</v-toolbar-title>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Bola chat 👋</v-toolbar-title>
     </v-app-bar>
+    <side-navi :drawer="drawer" @close-navi="closeNavi()"></side-navi>
     <v-main>
       <router-view></router-view>
     </v-main>
@@ -11,7 +13,18 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import SideNavi from "@/components/SideNavi.vue";
 
-@Component
-export default class App extends Vue {}
+@Component({
+  components: {
+    SideNavi,
+  },
+})
+export default class App extends Vue {
+  drawer: boolean = false;
+
+  closeNavi(): void {
+    this.drawer = false;
+  }
+}
 </script>
---
+++ packages/apps/src/components/SideNavi.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-navigation-drawer
+    v-model="isOpednDrawer"
+    absolute
+    left
+    width="85%"
+    temporary
+    class="fixed_nav"
+  >
+    <v-list nav dense>
+      <v-list-item-group active-class="deep-purple--text text--accent-4">
+        <v-list-item>
+          <v-list-item-title @click="signIn()">SIGN IN</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class SideNavi extends Vue {
+  @Prop({ type: Boolean, required: true }) drawer!: boolean;
+  childDrawer: boolean = false;
+
+  get isOpednDrawer(): boolean {
+    return this.drawer;
+  }
+
+  set isOpednDrawer(val: boolean) {
+    // TODO: 親子でdraweのフラグ管理はしたくない
+    this.childDrawer = !this.childDrawer;
+    if (!this.childDrawer) {
+      this.$emit("close-navi");
+    }
+  }
+
+  signIn(): void {
+    // TODO signIn処理の実装
+  }
+}
+</script>
+
+<style scoped>
+.fixed_nav {
+  position: fixed;
+}
+</style>
