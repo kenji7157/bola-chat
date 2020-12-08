59e29d72659cdd754fc903f82e4de27781139134
kenji7157 <kawake.kk@gmail.com>
2020-12-08 16:19:35 +0900

    テストファイルの生成

---
+++ packages/apps/src/test/practice.spec.ts
@@ -0,0 +1,8 @@
+import { isPositiveNumber } from './practice';
+
+describe('isPositiveNumberのテスト', () => {
+  test('正の場合にtrueとなること', () => {
+    const result = isPositiveNumber(10);
+    expect(result).toBeTruthy();
+  });
+});
---
+++ packages/apps/src/test/practice.ts
@@ -0,0 +1,7 @@
+export function isPositiveNumber(num: number) {
+  if (num > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
